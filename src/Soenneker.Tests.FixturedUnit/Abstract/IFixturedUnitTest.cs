7944e7d26afa06f42914e26f07daf84280c9037d
soenneker <jake@soenneker.com>
2026-08-29 14:17:24 -0500

    Improve public API XML documentation

--- src/Soenneker.Tests.FixturedUnit/Abstract/IFixturedUnitTest.cs
+++ src/Soenneker.Tests.FixturedUnit/Abstract/IFixturedUnitTest.cs
@@ -12,9 +12,23 @@ namespace Soenneker.Tests.FixturedUnit.Abstract;
 /// </summary>
 public interface IFixturedUnitTest : IAsyncDisposable
 {
+    /// <summary>
+    /// Resolves fixtured Unit Test.
+    /// </summary>
+    /// <typeparam name="T">Type of value handled by the Fixtured Unit Test.</typeparam>
+    /// <param name="scoped">Whether scoped.</param>
+    /// <returns>The resulting value.</returns>
     T Resolve<T>(bool scoped = false);
 
+    /// <summary>
+    /// Creates scope for the Fixtured Unit Test.
+    /// </summary>
     void CreateScope();
 
+    /// <summary>
+    /// Waits for on Queue To Empty for the Fixtured Unit Test.
+    /// </summary>
+    /// <param name="cancellationToken">Token used to cancel the operation.</param>
+    /// <returns>A task that completes when the wait on queue to empty operation is complete.</returns>
     ValueTask WaitOnQueueToEmpty(CancellationToken cancellationToken = default);
 }
